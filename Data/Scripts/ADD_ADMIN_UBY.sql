ae3386528bba0aac2167d65e9490b6667424d994
kbar163 <kabarrantes163@gmail.com>
2022-11-13 21:20:05 -0600

    Procedimiento almacenado ADD_UBY_ADMIN
    * Procedimiento almacenado creado para insertar un nuevo usuario administrador de ubyTEC en la base de datos, haciendo uso de todas las tablas necesarias para ello.

---
+++ Data/Scripts/ADD_ADMIN_UBY.sql
@@ -0,0 +1,38 @@
+--Procedimiento almacenado creado en la base de datos
+-- ubytecdb
+--Entradas:
+-- Todos los datos correspondientes a los atributos
+--del administrador uby en la base de datos, mas
+--un atributo de numero telefonico que se inserta en
+-- la tabla admin_uby_telefonos
+--Proceso: Se hace un insert con todos los datos
+--del administrador en la tabla administrador_uby,
+--seguido de un insert del numero de telefono y cedula
+--del administrador_uby en la tabla admin_uby telefonos
+
+CREATE OR REPLACE PROCEDURE ADD_UBY_ADMIN(
+	CEDULA_ADMIN_U VARCHAR(9),
+	NOMBRE_ADMIN_U VARCHAR(50),
+	APELLIDO1_ADMIN_U VARCHAR(50),
+	APELLIDO2_ADMIN_U VARCHAR(50),
+	CORREO_ADMIN_U VARCHAR(50),
+	USR_ADMIN_U VARCHAR(50),
+	PASS_ADMIN_U VARCHAR(50),
+	PROVINCIA_ADMIN_U VARCHAR(50),
+	CANTON_ADMIN_U VARCHAR(50),
+	DISTRITO_ADMIN_U VARCHAR(50),
+	TELEFONO_ADMIN_U VARCHAR(8))
+language plpgsql
+as $$
+begin
+-- stored procedure body
+INSERT INTO ADMINISTRADOR_UBY
+VALUES
+(CEDULA_ADMIN_U,NOMBRE_ADMIN_U,APELLIDO1_ADMIN_U,APELLIDO2_ADMIN_U,
+CORREO_ADMIN_U,USR_ADMIN_U,PASS_ADMIN_U,PROVINCIA_ADMIN_U,CANTON_ADMIN_U,
+DISTRITO_ADMIN_U);
+
+INSERT INTO ADMIN_UBY_TELEFONOS
+VALUES
+(DEFAULT,CEDULA_ADMIN_U,TELEFONO_ADMIN_U);
+end; $$
