4b5c097761d3e8ae4777cccee98943711ee19a4a
kbar163 <kabarrantes163@gmail.com>
2022-11-23 00:45:38 -0600

    Deployment try II

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using UbyTECService.Data.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ubytecdbContext>();
 builder.Services.AddMvc()
