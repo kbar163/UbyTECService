6f77d537518a8fe5505c7c0426a2ae2a99f7b24a
kbar163 <kabarrantes163@gmail.com>
2022-11-23 00:36:30 -0600

    Deployment try

--- Data/Repositories/AfiliateRepository.cs
+++ Data/Repositories/AfiliateRepository.cs
@@ -260,8 +260,6 @@ namespace UbyTECService.Data.Repositories
             }
 
             return response;
-            
-            
         }
 
         public ActionResponse ReplaceAfiAdmin(ReplaceAdminRequest newAdmin)
